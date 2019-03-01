7c9b9f73917585ed43e855f6efa723ede9616003
Vera Prinsen <vera.prinsen@nedap.com>
2019-03-01 09:30:11 +0100

    Revised documentation and changed __add__ method in graph.py

--- algorithms/color_refinement.py
+++ algorithms/color_refinement.py
@@ -4,25 +4,26 @@ from typing import List
 
 def color_refinement(G: "Graph"):
     """
-    Given a graph G, this method changes the colornum property of the vertices in the graph to be unique for every vertex
-    that could not be mapped to other vertices of the same colornum.
+    Given graph G, this method changes the colornum property of the vertices in the graph, so that all vertices with
+    the same colornum could be mapped to each other while structurally remaining the same graph
     :param G: Graph to be colored
-    :return: Colored graph
+    :return: Finished colored graph
     """
 
+    # In the initialization a dictionary 'colors' is created in which the keys are the 'colornum' property of vertices
+    # and the values are lists of vertices with that property colornum, max_colornum is the largest colornum used in
+    # the dictionary at that moment
     colors, max_colornum = __initialize_colors(G)
 
     has_changed = True
     while has_changed:
         has_changed = False
         for colornum, vertices in colors.copy().items():
-            max_colornum += 1
-
             if len(vertices) == 1:
                 continue
 
-            if __colorgroup_refinement(colors, colornum, vertices, max_colornum):
-                has_changed = True
+            max_colornum += 1
+            has_changed = __colorgroup_refinement(colors, colornum, vertices, max_colornum) or has_changed
     return G
 
 
@@ -30,7 +31,7 @@ def __initialize_colors(G: "Graph"):
     """
     Initializes the colornum properties of all vertices in graph G.
     :param G: Graph to be initialized
-    :return colors: Dictionary with colornums as keys and vertices as values
+    :return colors: Dictionary with colornums as keys and a list of vertices as values
     :return max_colornum: The current maximum colornum key in the dictionary 'colors'
     """
     colors = {}
@@ -46,21 +47,22 @@ def __initialize_colors(G: "Graph"):
 def __colorgroup_refinement(colors, colornum, vertices: List["Vertex"], next_colornum):
     """
     For vertices with property colornum = 'colornum', define which vertices have equal neighbours and which do not
-    If vertices within this group have not the same neighbours, these vertices will be given a new colornum and put in
-    a new dictionary key
+    If a vertex within this group does not have the same neighbours as the first vertex in this group, these vertices
+    will be given a new colornum 'next_colornum' and are moved from key colornum to next_colornum in the dictionary
     :param colors: Dictionary with property colornum as key and a list of vertices with that property as value
     :param colornum: Current colornum to be evaluated
     :param vertices: Vertices of that colornum to be evaluated
-    :param next_colornum: If vertices are different, give those vertices the next_colornum as property colornum
+    :param next_colornum: The next colornum that was not in colors as a key before this colorgroup refinement
+    :return has_changed: Returns if the colorgroup refinement has changed some colornum values for vertices
     """
 
-    # Creates a list of alle colornums of the neighbours of the first vertex in 'vertices'
+    # Creates a list of all colornums of the neighbours of the first vertex in 'vertices'
     v0 = vertices[0]
     v0_colors = []
     for v in v0.neighbours:
         v0_colors.append(v.colornum)
 
-    # Compare the neighbours of each vertex in 'vertices' with the first vertex in 'vertices'
+    # Compare each vertex other than the first vertex in 'vertices' with the first vertex in 'vertices'
     has_changed = False
     for v in vertices[1:]:
         if not __neighbours_equal(v0_colors.copy(), v):
@@ -74,7 +76,7 @@ def __colorgroup_refinement(colors, colornum, vertices: List["Vertex"], next_col
 
 def __neighbours_equal(v0_colors: List[int], v: Vertex):
     """
-    Determines if the colornum properties of the neighbours of vertex 'v' agrees with the list of colornum properties in
+    Determines if the colornum properties of the neighbours of vertex 'v' matches with the list of colornum properties in
     the list v0_colors
     :param v0_colors: List of colornum properties of the neighbours of the vertex to be compared with
     :param v: The vertex that needs to be compared with vertex v0
