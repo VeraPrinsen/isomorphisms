760bc464150867f10974ef575a42b00d7998af60
mvandermade <33425497+mvandermade@users.noreply.github.com>
2019-03-12 10:02:54 +0100

    Update main.py
    load_graph -> load_graph_list
    L[0] -> graphs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from input_output.file_output import load_graph_list
 from supporting_components.graph_io import *
 from algorithms.color_refinement import color_refinement
 from algorithms.color_initialization import degree_color_initialization
@@ -8,10 +9,9 @@ file = 'colorref_smallexample_4_16'
 # file = 'colorref_smallexample_2_49'
 filename = 'test_graphs/color_refinement/' + file + '.grl'
 
-with open(filename) as f:
-    L = load_graph(f, read_list=True)
+graphs = load_graph_list(filename)
 
-for i in range(0, len(L[0])):
+for i in range(0, len(graphs)):
     outputfilename = 'output_graphs/' + file + '_' + str(i) + '.dot'
     with open(outputfilename, 'w') as g0:
-        write_dot(color_refinement(degree_color_initialization(L[0][i])), g0)
+        write_dot(color_refinement(degree_color_initialization(graphs[i])), g0)
