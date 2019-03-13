b01e6181c84d507f8ed8020839ace70566bdbf49
mvandermade <33425497+mvandermade@users.noreply.github.com>
2019-03-13 15:08:42 +0100

    Refactor output_graph to output_files in main.py and file_output.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from input_output.file_output import load_graph_list
 from supporting_components.graph_io import *
 from algorithms.color_refinement import color_refinement
 from algorithms.color_initialization import degree_color_initialization
+import os
 
 # file = 'colorref_smallexample_4_7'
 file = 'colorref_smallexample_4_16'
@@ -11,9 +12,9 @@ filename = 'test_graphs/color_refinement/' + file + '.grl'
 
 graphs = load_graph_list(filename)
 
-os.makedirs(os.path.dirname('output_graphs/main/'), exist_ok=True)
+os.makedirs(os.path.dirname('output_files/main/'), exist_ok=True)
 
 for i in range(0, len(graphs)):
-    outputfilename = 'output_graphs/main/' + file + '_' + str(i) + '.dot'
+    outputfilename = 'output_files/main/' + file + '_' + str(i) + '.dot'
     with open(outputfilename, 'w') as g0:
         write_dot(color_refinement(degree_color_initialization(graphs[i])), g0)
