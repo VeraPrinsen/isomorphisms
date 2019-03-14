b25ee25898df5e31eafb14be2244c9a48da57f4d
mvandermade <33425497+mvandermade@users.noreply.github.com>
2019-03-14 21:23:40 +0100

    Update decide_gi.py
    rewrite the decide_gi to be a .test() function which returns bool if the test has passed True or False. The function itself writes to commandline and CSV.
    A link to the CSV is presented to the user at the beginning of the test (PyCharm can open this link). Because only faults are logged, the progress is visible by checking the contents of the CSV.

--- tests/decide_gi.py
+++ tests/decide_gi.py
@@ -1,39 +1,12 @@
-from input_output.file_output import load_graph_list
+import time
+
+from input_output.file_output import load_graph_list, create_csv_file, write_csv_line
 from input_output.sys_output import fail
+from input_output.sys_output import passed
 from supporting_components.graph_io import *
 from algorithms.decide_gi import is_balanced_or_bijected
 from algorithms.color_initialization import degree_color_initialization
 from algorithms.color_refinement import color_refinement
-
-# Expected test results
-total_tests = 22
-
-files = ['colorref_smallexample_2_49', 'colorref_smallexample_4_7', 'colorref_smallexample_6_15']
-
-# Dictionary containing expected outcomes.
-# Sourced from canvas.utwente.nl
-# Structure:
-# The dictionary is keyed on filename (str)
-# This dictionary contains for each key:
-#   A dictionary keyed on graph no. (int)
-#   This dictionary contains:
-#       A dictionary container on graph no. (int)
-#       This dictionary contains:
-#           Tuple containing : (is_balanced, is_bijected) ((bool, bool))
-
-expected_results = dict()
-expected_results['colorref_smallexample_2_49'] = {0: {1: (True, True)}}
-expected_results['colorref_smallexample_4_7'] = {0:  {1: (False, False), 2: (True, False), 3: (False, False)},
-                                                 1:  {2: (False, False), 3: (True, True)},
-                                                 2:  {3: (False, False)}
-                                                 }
-expected_results['colorref_smallexample_6_15'] = {0: {1: (True, True), 2: (False, False), 3: (False, False), 4: (False, False), 5: (False, False)},
-                                                  1: {2: (False, False), 3: (False, False), 4: (False, False), 5: (False, False)},
-                                                  2: {3: (True, True), 4: (False, False), 5: (False, False)},
-                                                  3: {4: (False, False), 5: (False, False)},
-                                                  4: {5: (True, False)}
-                                                  }
-
 
 def test_balanced_or_bijected(graph: 'Graph', is_balanced: 'Bool', is_bijected: 'Bool'):
     """
@@ -46,48 +19,119 @@ def test_balanced_or_bijected(graph: 'Graph', is_balanced: 'Bool', is_bijected: 
     test_is_balanced, test_is_bijected = is_balanced_or_bijected(graph)
 
     if not (test_is_balanced == is_balanced and test_is_bijected == is_bijected):
-        raise ValueError('             is_balanced  is_bijected' +
-                         '\nShould be  :     {}         {}'.format(is_balanced, is_bijected) +
-                         '\nCalculated :     {}         {}'.format(test_is_balanced, test_is_bijected)
+        raise ValueError('LOGGING: ' +
+                         'is_balanced was: {} ({})... '.format(test_is_balanced, is_balanced) +
+                         'is_bijected was: {} ({})...'.format(test_is_bijected, is_bijected)
                          )
     else:
         return True
 
 
-# Testing loop
-# A disjoint union is coloured using color_refinement(degree_color_initialization(graph))
-# The method is_balanced_or_bijected is tested against a hardcoded dictionary containing the expected outcomes.
-#
-# Currently the following graph files are tested:
-#     - colorref_smallexample_2_49
-#     - colorref_smallexample_4_7
-#     - colorref_smallexample_6_15
+def test():
+    testname = 'decide_gi'
+    csv_filepath = create_csv_file(testname)
+    print('<'+testname+'> '+ 'Appending to CSV: ' + "file:///"+csv_filepath.replace('\\', '/') + '\nStart...')
 
-error_count = 0
-graph_count = 0
-for file in files:
-    filename = '../test_graphs/color_refinement/' + file + '.grl'
-    graphs = load_graph_list(filename)
-    for i in range(0, len(graphs)):
-        for j in range(0 , i):
-            graph_count += 1
-            graph = graphs[i] + graphs[j]
-            color_refinement(degree_color_initialization(graph))
-            try:
-                is_balanced_or_bijected_test_result = test_balanced_or_bijected(graph, expected_results[file][j][i][0], expected_results[file][j][i][1])
-            except ValueError as err:
-                error_count += 1
-                print('---------------------------')
-                print("Statistics of " + file + "-" + str(i) + "_" + str(j) + ":")
-                fail("{0}".format(err))
-                print('')
-print('---------------------------')
-print("Statistics of test is_balanced_or_bijected:")
-print("Amount of graphs tested: " + str(graph_count) + "/" + str(total_tests))
-print("Amount of graphs failed: " + str(error_count))
-print('')
-if error_count > 0 or not total_tests == graph_count:
-    fail('TEST FAILED')
-else:
-    print('TEST PASSED')
-print('')
+    write_csv_line(csv_filepath, ['file', 'j', 'i', 'Pass?', 'Time (s)'])
+
+    # Expected test results
+    total_tests = 22
+
+    files = ['colorref_smallexample_2_49', 'colorref_smallexample_4_7', 'colorref_smallexample_6_15']
+
+    # Dictionary containing expected outcomes.
+    # Sourced from canvas.utwente.nl
+    # Structure:
+    # The dictionary is keyed on filename (str)
+    # This dictionary contains for each key:
+    #   A dictionary keyed on graph no. (int)
+    #   This dictionary contains:
+    #       A dictionary container on graph no. (int)
+    #       This dictionary contains:
+    #           Tuple containing : (is_balanced, is_bijected) ((bool, bool))
+
+    expected_results = dict()
+    expected_results['colorref_smallexample_2_49'] = {0: {1: (True, True)}}
+    expected_results['colorref_smallexample_4_7'] = {0: {1: (False, False), 2: (True, False), 3: (False, False)},
+                                                     1: {2: (False, False), 3: (True, True)},
+                                                     2: {3: (False, False)}
+                                                     }
+    expected_results['colorref_smallexample_6_15'] = {
+        0: {1: (True, True), 2: (False, False), 3: (False, False), 4: (False, False), 5: (False, False)},
+        1: {2: (False, False), 3: (False, False), 4: (False, False), 5: (False, False)},
+        2: {3: (True, True), 4: (False, False), 5: (False, False)},
+        3: {4: (False, False), 5: (False, False)},
+        4: {5: (True, False)}
+        }
+
+    # Testing loop
+    # A disjoint union is coloured using color_refinement(degree_color_initialization(graph))
+    # The method is_balanced_or_bijected is tested against a hardcoded dictionary containing the expected outcomes.
+    #
+    # Currently the following graph files are tested:
+    #     - colorref_smallexample_2_49
+    #     - colorref_smallexample_4_7
+    #     - colorref_smallexample_6_15
+
+    error_count = 0
+    graph_count = 0
+    total_time = 0
+
+    for file in files:
+        filename = 'test_graphs/color_refinement/' + file + '.grl'
+        graphs = load_graph_list(filename)
+
+        for i in range(0, len(graphs)):
+            for j in range(0, i):
+                t0 = time.time()
+                graph_count += 1
+
+                # Create disjoint union
+                graph = graphs[i] + graphs[j]
+
+                # Color refinement with degree coloring initialization
+                color_refinement(degree_color_initialization(graph))
+
+                # Try catch for test_balanced_or_bijected raises ValueError
+                try:
+                    is_balanced_or_bijected_test_result = test_balanced_or_bijected(graph, expected_results[file][j][i][0], expected_results[file][j][i][1])
+
+                    t1 = time.time() - t0
+                    pass_line = [file, str(j), str(i), str(is_balanced_or_bijected_test_result), "{0:.3f}".format(t1)]
+                    write_csv_line(csv_filepath, pass_line)
+
+                except ValueError as err:
+                    error_count += 1
+
+                    t1 = time.time() - t0
+                    fail_line = [file, str(j), str(i), False, "{0:.3f}".format(t1), "{0}".format(err)]
+                    write_csv_line(csv_filepath, fail_line)
+                    print(*fail_line, sep='\t', end='')
+                    fail('! Error #'+str(error_count))
+
+                total_time += t1
+
+
+    # Determine test outcome
+    test_pass_bool = False
+    if error_count == 0 and total_tests == graph_count:
+        test_pass_bool = True
+        passed(
+            '#Tests\t#Fail\tPASS?\tTime(s)\tTestfile:\n{0}/{1}\t{2}\t\t{3}\t{4}\tdecide_gi.py'.format(str(graph_count),
+                                                                    str(total_tests), str(error_count),
+                                                                    str(test_pass_bool), "{0:.3f}".format(total_time))
+            )
+    else:
+        fail(
+            '#Tests\t#Fail\tPASS?\tTime(s)\tTestfile:\n{0}/{1}\t{2}\t\t{3}\t{4}\tdecide_gi.py'.format(str(graph_count),
+                                                                    str(total_tests), str(error_count),
+                                                                    str(test_pass_bool), "{0:.3f}".format(total_time))
+            )
+
+    # Test summary
+    write_csv_line(csv_filepath, ['', '', '', ''])
+    write_csv_line(csv_filepath, ['#Tests', '#Fail', 'PASS?', 'Time (s)'])
+    write_csv_line(csv_filepath, [str(graph_count) + "/" + str(total_tests), str(error_count), str(test_pass_bool), "{0:.3f}".format(total_time)])
+
+    print('</' + testname + '>')
+    return test_pass_bool
