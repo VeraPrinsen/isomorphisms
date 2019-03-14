ade75bcd22ce732a1adaeef1b64c80c52288e38e
mvandermade <33425497+mvandermade@users.noreply.github.com>
2019-03-14 11:05:52 +0100

    Update main.py
    Additional comments and automatically create output dir for dot files

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from algorithms.color_initialization import degree_color_initialization
 from input_output.file_output import write_csv_line
 from input_output.file_output import create_csv_file
 import time
+import os
 
 # file = 'colorref_smallexample_4_7'
 file = 'colorref_smallexample_4_16'
@@ -14,28 +15,34 @@ filename = 'test_graphs/color_refinement/' + file + '.grl'
 
 graphs = load_graph_list(filename)
 
+# Test output csv creation
 csv_filepath = create_csv_file('main')
+# Write first row with column names
 write_csv_line(csv_filepath, ['file', 'i', 'time (s)', 'result'])
 
+# Test result accumulators
 end_time_sum = 0
 test_result_passed = True
 
+# main test loop
 for i in range(0, len(graphs)):
-    outputfilename = 'output_graphs/' + file + '_' + str(i) + '.dot'
+    outputfilename = 'output_files/main/' + file + '_' + str(i) + '.dot'
+    os.makedirs(os.path.dirname(outputfilename), exist_ok=True)
 
     start_time: float = time.time()
     with open(outputfilename, 'w') as g0:
         write_dot(color_refinement(degree_color_initialization(graphs[i])), g0)
 
     test_result = True
-
     if not test_result:
         test_result_passed = False
 
     end_time = time.time() - start_time
     end_time_sum = end_time_sum + end_time
+    # Write individual test result to the csv
     write_csv_line(csv_filepath, [file, str(i), "{0:.3f}".format(end_time), str(test_result)])
 
+# Write the total test result row to csv
 write_csv_line(csv_filepath, ['', '', '', ''])
 write_csv_line(csv_filepath, ['', '', 'Total (s)', 'All pass?'])
 write_csv_line(csv_filepath, ['', '', "{0:.3f}".format(end_time_sum), str(test_result_passed)])
