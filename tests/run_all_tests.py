ba73fc0580d8e2ef02892cd1a701958d8df61608
mvandermade <33425497+mvandermade@users.noreply.github.com>
2019-03-20 15:17:14 +0100

    Update run_all_tests.py
    Added Fast Color Refinement (FCS) and Color Refinement test to decide.gy

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,11 +1,33 @@
 from input_output.sys_output import passed, fail
-from tests import decide_gi
+from tests.decide_gi import Decide_gi
+from tests import fun_provider
 
-if decide_gi.test() and decide_gi.test():
-    print('')
-    print('')
-    passed('---> All test passed, terminated successfully!')
+all_pass_flag = True
+
+# Add functions to test by constructing objects in a list
+functions_to_test = [
+    Decide_gi()
+]
+
+# Types of tests to run on test objects
+test_types_to_run = [
+    'run_color_refinement',
+    'run_fast_color_refinement'
+]
+
+for test_object in functions_to_test:
+    if 'run_color_refinement' in test_types_to_run:
+        if not test_object.run_color_refinement(fun_provider.Default_color_refinement()):
+            all_pass_flag = False
+
+    if 'run_fast_color_refinement' in test_types_to_run:
+        if not test_object.run_fast_color_refinement(fun_provider.Fast_color_refinement()):
+            all_pass_flag = False
+
+
+# Finally
+print('')
+if all_pass_flag:
+    passed('---> All test passed!')
 else:
-    print('')
-    print('')
     fail('---> Not all tests passed, aborted.')
