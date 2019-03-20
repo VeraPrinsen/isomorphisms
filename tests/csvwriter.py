d2c742eaf2b2ed1aef84d78206e5b52e04415334
mvandermade <33425497+mvandermade@users.noreply.github.com>
2019-03-20 16:49:40 +0100

    Update csvwriter.py
    Added .unittest() for Cvwriter object.

--- tests/csvwriter.py
+++ tests/csvwriter.py
@@ -5,66 +5,74 @@ from input_output.file_output import write_csv_line
 from input_output.file_output import create_csv_file
 import time
 import csv
+from input_output.sys_output import fail, passed
 
 # This file is to demonstrate the workings of writing test results to a CSV file.
 # The test itself is a mock and will always pass.
 # The result of the test are written to CSV in the folder output_files/csv/
-from input_output.sys_output import fail, passed
 
-file = 'colorref_smallexample_4_16'
-filename = 'test_graphs/color_refinement/' + file + '.grl'
-graphs = load_graph_list(filename)
+class Csvwriter:
+    def unittest(self):
+        return _unittest()
 
-# Output csv creation
-csv_filepath = create_csv_file('csvwriter_test')
-# Write first row with column names
-csv_write_array = [['file', 'i', 'time (s)', 'result']]
 
-# Test result accumulators
-end_time_sum = 0
-test_result_passed = True
+def _unittest():
+    file = 'colorref_smallexample_4_16'
+    filename = 'test_graphs/color_refinement/' + file + '.grl'
+    graphs = load_graph_list(filename)
 
-# mocked test loop
-for i in range(0, len(graphs)):
-    outputfilename = 'output_files/main/' + file + '_' + str(i) + '.dot'
+    # Output csv creation
+    csv_filepath = create_csv_file('csvwriter_test')
+    # Write first row with column names
+    csv_write_array = [['file', 'i', 'time (s)', 'result']]
 
-    start_time = time.time()
-    save_graph_as_dot(color_refinement(degree_color_initialization(graphs[i])), outputfilename)
+    # Test result accumulators
+    end_time_sum = 0
+    test_result_passed = True
 
-    # In a real test this value might be false...
-    test_result_mock = True
+    # mocked test loop
+    for i in range(0, len(graphs)):
+        outputfilename = 'output_files/main/' + file + '_' + str(i) + '.dot'
 
-    if not test_result_mock:
-        test_result_passed = False
+        start_time = time.time()
+        save_graph_as_dot(color_refinement(degree_color_initialization(graphs[i])), outputfilename)
 
-    end_time = time.time() - start_time
-    end_time_sum = end_time_sum + end_time
-    # Write individual test result to the csv
-    csv_write_array.append([file, str(i), "{0:.3f}".format(end_time), str(test_result_mock)])
+        # In a real test this value might be false...
+        test_result_mock = True
 
-# Write the total test result row to csv
-csv_write_array.append(['', '', '', ''])
-csv_write_array.append(['', '', 'Total (s)', 'All pass?'])
-csv_write_array.append(['', '', "{0:.3f}".format(end_time_sum), str(test_result_passed)])
+        if not test_result_mock:
+            test_result_passed = False
 
-for line in csv_write_array:
-    write_csv_line(csv_filepath, line)
+        end_time = time.time() - start_time
+        end_time_sum = end_time_sum + end_time
+        # Write individual test result to the csv
+        csv_write_array.append([file, str(i), "{0:.3f}".format(end_time), str(test_result_mock)])
 
-print('CSV written to: ' + csv_filepath)
+    # Write the total test result row to csv
+    csv_write_array.append(['', '', '', ''])
+    csv_write_array.append(['', '', 'Total (s)', 'All pass?'])
+    csv_write_array.append(['', '', "{0:.3f}".format(end_time_sum), str(test_result_passed)])
 
-print("VERIFYING write action...")
+    for line in csv_write_array:
+        write_csv_line(csv_filepath, line)
 
-csv_read_array =[]
+    print('CSV written to: ' + csv_filepath)
 
-with open(csv_filepath, newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        csv_read_array.append(row)
+    print("VERIFYING write action...")
 
-# The first row is the separator line, pop it
-csv_read_array.pop(0)
+    csv_read_array =[]
 
-if csv_read_array == csv_write_array:
-    passed("CSV write OK")
-else:
-    fail("CSV write test failed")
+    with open(csv_filepath, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            csv_read_array.append(row)
+
+    # The first row is the separator line, pop it
+    csv_read_array.pop(0)
+
+    if csv_read_array == csv_write_array:
+        passed("CSV write OK")
+        return True
+    else:
+        fail("CSV write test failed")
+        return False
