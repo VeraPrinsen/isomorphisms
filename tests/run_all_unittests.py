643dcd7c420614791cd5a3301587bacd9838f57a
mvandermade <33425497+mvandermade@users.noreply.github.com>
2019-03-20 16:49:02 +0100

    Update run_all_unittests.py
    renamed tests to unit tests. Simplified testing approach. Only call .unittest() and expect bool as return.

---
+++ tests/run_all_unittests.py
@@ -0,0 +1,24 @@
+from input_output.sys_output import passed, fail
+from tests.decide_gi import Decide_gi
+from tests.csvwriter import Csvwriter
+from tests import fun_provider
+
+all_pass_flag = True
+
+# Add functions to test by constructing objects in a list
+functions_to_test = [
+    Decide_gi(),
+    Csvwriter(),
+
+]
+
+for test_object in functions_to_test:
+    if not test_object.unittest():
+        all_pass_flag = False
+
+# Finally
+print('')
+if all_pass_flag:
+    passed('---> All test passed!')
+else:
+    fail('---> Not all tests passed, aborted.')
