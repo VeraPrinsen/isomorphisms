442a12e8014f7d8943efa41876d7a59a8fc84ae7
mvandermade <33425497+mvandermade@users.noreply.github.com>
2019-03-28 15:20:48 +0100

    documentation of run all unittests

--- tests/run_all_unittests.py
+++ tests/run_all_unittests.py
@@ -2,11 +2,24 @@ from input_output.sys_output import passed, fail
 from tests import decide_gi, csvwriter, color_refinement, fast_color_refinement, graph, graph_del_vertex_edge, \
     isomorphism_problem, preprocessing_twins
 
+"""
+All unit tests will be called in sequence.
+It is up to the unit test to display useful information about the test either to the console (stdout) or CSV.
+
+Procedure:
+By calling the method <imported test>.unittest(write_csv_any, write_stdout_passed, write_stdout_fail) a unit test is run.
+A boolean value that is returned by the function .unittest() is stored in a list called `result_boolean`.
+
+After all the unit test results are appended to `result_boolean`, the list is searched for any occurences of False.
+If any False is detected in the list, the user is notified.
+
+"""
+
+# Global test settings
 write_csv_any = True
 write_stdout_passed = False
 write_stdout_fail = True
 
-all_pass_flag = True
 result_boolean = list()
 
 result_boolean.append(color_refinement.unit_test(write_csv_any, write_stdout_passed, write_stdout_fail))
