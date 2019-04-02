5677ac553a695e20023cad3f482fb423d3d951c0
Janine Klein Rot <janine.kleinrot@nedap.com>
2019-04-02 13:15:19 +0200

    Initialize colors in initial coloring of graph

--- algorithms/color_initialization.py
+++ algorithms/color_initialization.py
@@ -2,15 +2,19 @@ def degree_color_initialization(G: "Graph"):
     """
     Initializes the colornum properties of all vertices in graph G based on the degree of the vertices.
     Initializes the max_colornum property of the graph as well.
+    Initializes colors, the map with vertices grouped by color, as well.
     :param G: The graph to be initialized
     :return The graph with the initial coloring
     """
     max_colornum = 0
+    colors = {}
     for v in G.vertices:
         v.colornum = v.degree
+        colors.setdefault(v.colornum, list()).append(v)
         if v.colornum > max_colornum:
             max_colornum = v.colornum
     G.max_colornum = max_colornum
+    G.colors = colors
     return G
 
 
@@ -19,13 +23,17 @@ def twins_color_initialization(G: "Graph"):
     Initializes the colornum properties of all vertices in graph G based on the degree of the vertices when twins
     have been removed.
     Initializes the max_colornum property of the graph as well.
+    Initializes colors, the map with vertices grouped by color, as well.
     :param G: The graph to be initialized
     :return The graph with the initial coloring
     """
     max_colornum = 0
+    colors = {}
     for v in G.vertices:
         v.colornum = v.degree_fixed
+        colors.setdefault(v.colornum, list()).append(v)
         if v.colornum > max_colornum:
             max_colornum = v.colornum
     G.max_colornum = max_colornum
+    G.colors = colors
     return G
