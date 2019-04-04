9ddf9af4e6393d0e65524aa5d0948452746c5ef0
Janine Klein Rot <janine.kleinrot@nedap.com>
2019-04-04 13:41:55 +0200

    Deleted unnecessary data structure from backup and revert method from graph

--- supporting_components/graph.py
+++ supporting_components/graph.py
@@ -485,25 +485,23 @@ class Graph(object):
 
     def backup(self):
         """
-        Creates a backup of the color of each vertex in the graph, the maximum colornum and the color map of vertices grouped by color.
-        :return: Two structures with vertices and color and the maximum colornum
+        Creates a backup of the maximum colornum and the color map of vertices grouped by color.
+        :return: The maximum colornum and the color map
         """
-        color_list = {}
         colors = {}
         for v in self.vertices:
-            color_list[v.label] = v.colornum
             colors.setdefault(v.colornum, list()).append(v)
-        return color_list, self.max_colornum, colors
+        return self.max_colornum, colors
 
-    def revert(self, color_list: "Dict[int]", max_colornum: "int", colors: "Dict[int, List[Vertex]]"):
+    def revert(self, max_colornum: "int", colors: "Dict[int, List[Vertex]]"):
         """
         Convert a graph to the state of the arguments
-        :param color_list: The map of vertices with its color
         :param max_colornum: The maximum colornum
         :param colors: The map of color with its vertices
         """
-        for v in self.vertices:
-            v.colornum = color_list[v.label]
+        for color, vertices in colors.items():
+            for v in vertices:
+                v.colornum = color
         self.max_colornum = max_colornum
         self.colors = colors
 
