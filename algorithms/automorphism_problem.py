079dd8b6b43598a1650d3f02bf6a43a77a74b9d8
Vera Prinsen <vera.prinsen@nedap.com>
2019-04-06 10:46:05 +0200

    Improved order computation

--- algorithms/automorphism_problem.py
+++ algorithms/automorphism_problem.py
@@ -10,23 +10,27 @@ def order_computation(H: 'List[permutation]'):
     :param H: Permutation group
     :return: int with the order of the list of permutations
     """
+    # If H has only one element and it is the unity permutation. The length of the orbit is 1.
+    # Because whatever a you choose, it can only map to itself.
+    if len(H) == 1 and H[0].istrivial():
+        return 1
+
+    # And because the method Stabilizer does not return the unity permutation as part of the stabilizer,]
+    # if further down in the recursion the length of H is zero, this actually means that only the unity
+    # permutation is left, because that one must always be in the stabilizer. So in this case, the length
+    # of the orbit is also 1.
+    if len(H) == 0:
+        return 1
+
+    # In any other case, the theorem holds that the order of H is the order of the stabilizer multiplied by the
+    # length of the orbit ( |H| = |H0| * |0_H| ). For this theorem the theory of slide 21 of the lecture slides
+    # of L4 are used.
+
     # Choose a with nontrivial orbit
     a = FindNonTrivialOrbit(H)
-    # Determine orbit of a (list of vertices 'a' can be mapped to)
+    # Determine orbit of a (list of vertices to which 'a' can be mapped to)
     O_H = Orbit(H, a)
-
-    # If length of the list of permutation H is equal to 1 (which is the stabilizer of the previous recursive call):
-    # Length of orbit = length of elements in the permutation in H
-    # Stabilizer = empty set, because there is no permutation that leaves an element untouched
-    # And the order is equal to the orbit that is determined using the previous stabilizer
-    if len(H) == 1:
-        return len(O_H)
-
     # Determine stabilizer of a (list of permutations that leave 'a' untouched)
     H_0 = Stabilizer(H, a)
 
-    # Order of list of permutation H = |H|
-    # Order of stabilizer (which is a list of permutations) H_0 = |H_0|
-    # Length of orbit (which is a list of vertices labels) O_H = |O_H|
-    # Orbit-Stabilizer-theorem:     |H| = |H_0| * |O_H|
     return order_computation(H_0) * len(O_H)
